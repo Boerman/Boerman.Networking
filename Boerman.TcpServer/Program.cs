53f570e894eac2783ea41df3aa939a4aa97c5196
Corstian Boerman <corstian@Corstians-MBP.lan>
2017-11-18 20:31:16 +0100

    Fixed bug with tcpClient (unable to send more then one message)

--- Boerman.TcpServer/Program.cs
+++ Boerman.TcpServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 
 namespace Boerman.TcpServer
 {
@@ -8,7 +9,7 @@ namespace Boerman.TcpServer
         static void Main(string[] args)
         {
             // Create a new TcpServer listening on port 2626
-            var server = new TcpLib.Server.TcpServer(new IPEndPoint(IPAddress.Parse("0.0.0.0"), 2626));
+            var server = new TcpLib.Server.TcpServer(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2626));
 
             server.Connected += (sender, e) => {
                 Console.WriteLine($"{e.TimeStamp}: {e.Endpoint} connected");

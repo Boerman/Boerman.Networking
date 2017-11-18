18f2bbb9530db6d251a6675b9cbb0a2850a10e1e
Corstian Boerman <corstian@Corstians-MacBook-Pro.local>
2017-11-18 23:33:55 +0100

    Combined some files in one

--- Boerman.TcpLib/Client/ClientSettings.cs
+++ Boerman.TcpLib/Client/ClientSettings.cs
@@ -6,8 +6,6 @@ namespace Boerman.TcpLib.Client
     public class ClientSettings
     {
         public EndPoint     EndPoint                { get; set; }
-        public string       Splitter                { get; set; }
-        public int          Timeout                 { get; set; }
         public bool         ReconnectOnDisconnect   { get; set; }
         public Encoding     Encoding                { get; set; }
     }
--- Boerman.TcpLib/Server/ServerSettings.cs
+++ Boerman.TcpLib/Server/ServerSettings.cs
@@ -6,7 +6,6 @@ namespace Boerman.TcpLib.Server
     public class ServerSettings
     {
         public IPEndPoint   IpEndPoint      { get; set; }
-        public string       Splitter        { get; set; }
         public int          ClientTimeout   { get; set; }
         public bool         ReuseAddress    { get; set; }
         public Encoding     Encoding        { get; set; }
